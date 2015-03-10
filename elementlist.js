b07cdc23ac2416403683b8a8e847e75715e1a096
nathanhelenihi <nathan.helenihi@cloudbeds.com>
2015-03-10 16:02:44 -0700

    ota php sdk docs

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","MyAllocator\\phpsdkota\\src\\Api\\Inbound\\MaInboundInterface"],["c","MyAllocator\\phpsdkota\\src\\Api\\Inbound\\MaRouter"],["c","MyAllocator\\phpsdkota\\src\\Object\\MaError"],["c","MyAllocator\\phpsdkota\\src\\Object\\MaResponse"]];
